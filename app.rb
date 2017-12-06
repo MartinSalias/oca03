9e6074d1dc419a97adcccbc28dcc1aee96626a73
vagrant <vagrant@kleer-csd-box>
2017-12-06 11:28:47 -0300

    version1

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
 
 get '/' do
-    
+
+	#@@juego = Juego.new(rand(10),rand(10),"+")
+  @@juego = Juego.new(2,2,"+")
+  	  
+	erb :formulario
+
 end
+
+post "/resultado" do
+
+	@res = params["resultado"].to_i
+
+	@esvalido = @@juego.validarResultado(@res)
+
+	if @esvalido
+		"ok"
+	else
+		"Burro"
+	end
+	
+end
+
