056f5aa346ccf874d4c4abee1ac71cd01f9a3586
vagrant <vagrant@kleer-csd-box>
2017-12-06 12:57:05 -0300

    prueba 3

--- spec/juego_spec.rb
+++ spec/juego_spec.rb
@@ -2,9 +2,13 @@ require "./lib/juego.rb"
 
 describe "Juego" do
 
-	it "empieza juego" do
-			juego = Juego.new(2,3,"+")
-			juego.validarResultado(5).should == true
+	it "Suma correcta" do
+		juego = Juego.new(2,3,"+")
+		juego.validarResultado(5).should == true
 	end
 
+	it "Suma incorrecta" do
+		juego = Juego.new(2,3,"+")
+		juego.validarResultado(4).should == false
+	end		
 end
